ed3758ddad972bb2a3403531d42fa69eac0ffb08
mbpomilio <pomiliomb@gmail.com>
2022-06-30 21:00:28 -0300

    Segundo commit

---
+++ AppCoder/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+
+
+urlpatterns = [
+    path('curso/', curso),
+    path('cursos/', cursos),
+    path('profesores/', profesores),
+    path('estudiantes/', estudiantes),
+    path('entregables/', entregables),
+    path('', inicio),
+]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -8,3 +8,19 @@ def curso(request):
     curso.save()
     texto = f"Curso creado: {curso.nombre}, comision: {curso.comision}"
     return HttpResponse(texto)
+
+def inicio(request):
+    return render(request, "AppCoder/inicio.html")
+
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return render(request, "AppCoder/entregables.html")
+
